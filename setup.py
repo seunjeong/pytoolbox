a670981dbe66b2983c1721268428708e571e4538
Seongeun Jeong <jeongse@gmail.com>
2020-07-02 12:57:22 -0700

    v0.02

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytoolbox',
-    version='0.0.1',
+    version='0.0.2',
     description='Some useful functions for Python data analysis',
     url='https://github.com/seunjeong/pytoolbox.git',
     author='Seongeun Jeong',
