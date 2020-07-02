0a348bdb46ea01163f9293c5addbbb9a85b02df3
Seongeun Jeong <jeongse@gmail.com>
2020-07-02 12:33:41 -0700

    rm listdict.py fix2

--- pytoolbox/pandastool.py
+++ pytoolbox/pandastool.py
@@ -7,3 +7,17 @@ def find_max_each_row(df, max_col_name = 'max_val'):
     return df
     
 #df = pd.DataFrame(np.random.randn(5, 3), columns = ['x', 'y', 'z'], index=['a', 'c', 'e', 'f', 'h'])
+
+def two_list_to_dic(key_list, value_list):
+    """
+    @example: 
+    key_list = ['a', 'b', 'c']
+    value_list = range (3)
+    foo = two_list_to_dic (key_list, value_list)
+    """
+    zipped = zip(key_list, value_list)
+    
+    # Create a dictionary from zip object
+    dict_obj = dict(zipped)
+
+    return dict_obj
